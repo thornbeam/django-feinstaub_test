be9465d48f9cce748a86b2e326000b5e30e62e12
thornbeam <thornbeam@posteo.eu>
2024-02-11 20:44:17 +0100

    feat(commands): add command for importing data for a particular sensor type

---
+++ sensors/management/commands/import_data-mono.py
@@ -0,0 +1,68 @@
+from django.core.management import BaseCommand
+import csv
+
+from bs4 import BeautifulSoup
+import requests
+import urllib.request
+from datetime import date
+from datetime import timedelta
+import time
+from .modules.sensor_type import get_sensor_type
+from .modules.create_object import create
+
+# command example
+# python manage.py import_data-mono --url http://archive.sensor.community --date 2023-12-25 --type bme280
+
+class Command(BaseCommand):
+    help = 'Load data csv file into the database'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--url', type=str)
+        parser.add_argument('--date', type=str)
+        parser.add_argument('--type', type=str)
+
+    def handle(self, *args, **kwargs):
+
+        website = kwargs['url']
+        date = kwargs['date']
+        inserted_sensor_type = kwargs['type']
+        base_url = website + "/" + date
+        page = requests.get(base_url)
+        soup = BeautifulSoup(page.content, "html.parser")
+
+        start = time.time()
+        object_count = 0
+        for i in soup.find_all("a", href = True):
+            if date in i["href"]:
+                sensor_type = get_sensor_type(i['href'], date)
+                sensor_type = sensor_type.replace("-", "")
+
+                if sensor_type != None and inserted_sensor_type == sensor_type:
+                    url = base_url + "/" + i["href"]
+                    print("current url:", url)
+
+                    response = urllib.request.urlopen(url)
+                    lines = [line.decode("utf-8") for line in response.readlines()]
+                    reader = csv.reader(lines, delimiter=";")
+
+                    index = 0
+                    for row in reader:
+                        # ignore first header row
+                        if index == 0:
+                            index += 1
+
+                        else:
+                            # convert empty string to NaN to avoid type error
+                            for i in range(len(row)):
+                                if row[i] == "" or row[i] == "unavailable":
+                                    row[i] = "nan"
+
+                            create(sensor_type, row)
+
+                            object_count += 1
+                            print(str(object_count) + ". object created.")
+        print("total:", object_count, "objects")
+
+        end = time.time()
+        total_time = end - start
+        print("time:", total_time)
