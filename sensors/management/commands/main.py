89acde8238025e081394c48112e3742208eb964d
thornbeam <thornbeam@posteo.eu>
2024-03-16 10:41:40 +0100

    feat(commands): start to merge all parts of import data

---
+++ sensors/management/commands/main.py
@@ -0,0 +1,50 @@
+from django.core.management import BaseCommand
+import csv
+
+from bs4 import BeautifulSoup
+import requests
+import urllib.request
+from datetime import date
+from datetime import timedelta
+import time
+import json
+from .modules.sensor_type import get_sensor_type
+from .modules.sensor_type_queue import register, skip
+
+
+class Command(BaseCommand):
+    help = "Get sensor type from csv, generate json with sensor type names, generate models.py, admin.py"
+
+    def add_arguments(self, parser):
+        parser.add_argument("--url", type=str)
+        parser.add_argument("--json", type=str)
+        parser.add_argument("--app", type=str)
+        parser.add_argument("--date", type=str)
+
+    def handle(self, *args, **kwargs):
+        website = kwargs['url']
+        date = kwargs['date']
+
+        base_url = website + "/" + date
+        page = requests.get(base_url)
+        soup = BeautifulSoup(page.content, "html.parser")
+        sensor_type_queue = dict()
+
+        for a in soup.find_all("a", href=True):
+            sensor_type = get_sensor_type(a["href"], date)
+
+            if sensor_type != None:
+                url = base_url + "/" + a["href"]
+
+                if len(sensor_type_queue) == 0:
+                    register(sensor_type_queue, sensor_type, url)
+                else:
+                    if sensor_type not in sensor_type_queue.keys():
+                        register(sensor_type_queue, sensor_type, url)
+                    else:
+                        skip(sensor_type)
+
+        with open('sensor_csv_header.json', 'w') as file:
+            file.write(json.dumps(sensor_type_queue))
+        print("see in root path: ./sensor_csv_header.json")
+
